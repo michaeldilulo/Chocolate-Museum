98c775017372826fc7936b6431d1b110a52251cb
michael dilulo <snoleopard01001101@gmail.com>
2020-09-16 12:00:36 -0700

    Navbar change on scroll, mobile responsive

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ function mobileNavbarResponsive() {
         chocNavLinks.style.display = 'block'
     }
 }
+
+// Navbar color change 
+
+const header = document.querySelector('.chocolate-museum')
+
+window.onscroll = function () {
+    var top = window.scrollY;
+    if (top >= 100) {
+        header.classList.add('header-active')
+    } else {
+        header.classList.remove('header-active')
+    }
+}
